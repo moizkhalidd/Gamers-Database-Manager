1bb850d8aade7c0247985d66bd4a5db111de0aeb
moizkhalidd <mmoizk2004@gmail.com>
2024-10-29 03:19:59 +0500

    Add basic functions like search and add to the BST

--- DS_C_A3_i232552.cpp
+++ DS_C_A3_i232552.cpp
@@ -5,6 +5,7 @@ struct GamesPlayed
     string gameID;
     float hoursPlayed;
     int achievementsUnlocked;
+    GamesPlayed *next = nullptr;
 
     GamesPlayed(string id, float hours, int achievements)
     {
@@ -19,12 +20,20 @@ struct Game
 {
     int gameID;
     string gameName;
+    string developer;
+    string publisher;
+    float fileSize;
+    int copiesSold;
     Game *left = nullptr, *right = nullptr;
 
-    Game(int id, string name) 
+    Game(int id, string name, string dev, string pub, float size, int sold) 
     {
        gameID = id; 
-       gameName = name; 
+       gameName = name;
+       developer = dev;
+       publisher = pub;
+       fileSize = size;
+       copiesSold = sold;
     }
 };
 
@@ -50,9 +59,84 @@ struct Player
     void addGame(string gameID, float hours, int achievements) 
     {
         GamesPlayed *newGame = new GamesPlayed(gameID, hours, achievements);
+        newGame -> next = gamesHead;
         gamesHead = newGame;
     }
 };
+class Database 
+{
+  private:
+    Player *playerRoot = nullptr;
+    Game *gameRoot = nullptr;
+    
+    int generateSeed(string &rollNumber) 
+    {
+      int seed = 0;
+      for (int i = 0; i < rollNumber.length(); ++i) 
+      {
+        char ch = rollNumber[i];
+        if (ch >= '0' && ch <= '9') 
+        {
+            seed = seed * 10 + (ch - '0');
+        }
+      }
+
+      return seed;
+    }  
+    
+    
+    Player* searchPlayer(Player* root, int id) 
+    {
+        if (!root || root->playerID == id) 
+        return root;
+        
+        if (id < root->playerID) 
+        return searchPlayer(root->left, id);
+        
+        return searchPlayer(root->right, id);
+    }
+
+
+    Game* searchGame(Game* root, int id) 
+    {
+        if (!root || root->gameID == id) 
+        return root;
+        
+        if (id < root->gameID) 
+        return searchGame(root->left, id);
+        
+        return searchGame(root->right, id);
+    }
+    
+    
+    Player* insertPlayerHelper(Player* root, int id, string name, string phone, string email, string password) 
+    {
+        if (!root) 
+        return new Player(id, name, phone, email, password);
+        
+        if (id < root->playerID) 
+        root->left = insertPlayerHelper(root->left, id, name, phone, email, password);
+        else if (id > root->playerID) 
+        root->right = insertPlayerHelper(root->right, id, name, phone, email, password);
+        
+        return root;
+    }
+    
+    
+     Game* insertGameHelper(Game* root, int id, string name, string developer, string publisher, float size, int copiesSold) 
+     {
+        if (!root) 
+        return new Game(id, name, developer, publisher, size, copiesSold);
+        
+        if (id < root->gameID) 
+        root->left = insertGameHelper(root->left, id, name, developer, publisher, size, copiesSold);
+        else if (id > root->gameID) 
+        root->right = insertGameHelper(root->right, id, name, developer, publisher, size, copiesSold);
+        
+        return root;
+    }
+    
+};    
 int main()
 {
 
